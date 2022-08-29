be6ce727f52db7c060a8ddaad3b8904226ed10aa
Pritesh0_0 <patilpritesh347@gmail.com>
2022-08-29 22:54:12 +0530

    ffff

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import styles from "./Product.module.css"
 const Product = () => {
   const [products, setProducts] = React.useState([]);
 
@@ -18,26 +18,27 @@ const Product = () => {
       
           
             {products?.map((product) => (
-              <div>
-              <h3 data-cy="product-name">{product.name}</h3>
-              <h6 data-cy="product-description">{product.description}</h6>
+              <div className={styles.card}>
+              <h3 className = {styles.h3} data-cy="product-name">{product.name}</h3>
+              <h6 data-cy="product-description" className = {styles.h6}>{product.description}</h6>
+              <button data-cy="product-add-item-to-cart-button">Add to Cart</button>
+      <div>
+        <button data-cy="product-increment-cart-item-count-button">+</button>
+        <span data-cy="product-count">
+          {
+            // Count here from CartItems
+          }
+        </span>
+        <button data-cy="product-decrement-cart-item-count-button">-</button>
+        <button data-cy="product-remove-cart-item-button">Remove Item from Cart</button>
+      </div>
               </div>
             ))}
           
           
           
      
-      <button data-cy="product-add-item-to-cart-button"></button>
-      <div>
-        <button data-cy="product-increment-cart-item-count-button"></button>
-        <span data-cy="product-count">
-          {
-            // Count here from CartItems
-          }
-        </span>
-        <button data-cy="product-decrement-cart-item-count-button"></button>
-        <button data-cy="product-remove-cart-item-button"></button>
-      </div>
+      
       
           </div>
      
